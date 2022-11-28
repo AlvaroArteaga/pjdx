31577cd2e5720e2af61606d8e14b99d746f378d5
AlvaroArteaga <83927256+AlvaroArteaga@users.noreply.github.com>
2022-11-28 00:17:15 -0300

    ok

--- pjdx.py
+++ pjdx.py
@@ -16,10 +16,18 @@ from kivymd.toast import toast
 from pathlib import Path
 from configparser import ConfigParser
 configuracion = ConfigParser(allow_no_value=True)
+diccionario = ConfigParser(allow_no_value=True)
 #print(os.path.dirname(__file__))
 cfg_path = os.path.join(os.path.dirname(__file__),'config.cfg')
-#print(cfg_path)
+diccionario_path = os.path.join(os.path.dirname(__file__),'diccionarios.cfg')
+
+configuracion.optionxform = lambda option: option
 configuracion.read(cfg_path,encoding="utf8")
+
+diccionario.optionxform = lambda option: option
+diccionario.read(diccionario_path,encoding="utf8")
+
+
 RUTA=""
 import warnings
 import re
@@ -68,6 +76,7 @@ def limpieza_RUT(rut_revisar):
     #print(i)
     #print('--2--')
     #print(rut)
+    rut=rut_revisar
     if not pd.isnull(rut):
         rut=rut.strip() #elimina espacio al principio o final
         #print('--3--')
@@ -97,7 +106,7 @@ def limpieza_RUT(rut_revisar):
             rut=rut[:(len(rut)-9)]+"."+rut[(len(rut)-9):]
             #print(rut)
             #print('--10--')
-        if rut!='' or rut!='K':
+        if rut!='' and rut!='K':
             rut_revisar=rut
             #print('--11--')
             #rut_revisar.replace(rut_revisarrut)
@@ -111,12 +120,17 @@ def limpieza_RUT(rut_revisar):
         #print(rutx)
     return rut_revisar
    
-
+def aplicar_strip(x):
+    x=x.strip()
+   
         
 
 def pjdx_mensual(mes,ruta,anno):
+    print('-------1-------')
+    nan_value = float("NaN") 
     if mes!="Abril":
         exit()
+    print('-------2-------')
     hoja1='1_Cobro_Peajes'
     hoja2='2_Pago_Peajes'
     hoja3='3_Cambio_Regimen'
@@ -124,39 +138,79 @@ def pjdx_mensual(mes,ruta,anno):
     df1_total = pd.DataFrame()
     df2_total = pd.DataFrame()
     df3_total = pd.DataFrame()
-    for file in archivos:                         # loop through Excel files
+    print('-------3-------')
+    for file in archivos:
+        print('-------4-------')
         if file.endswith('.xlsx'):
+            print('-------5-------')
             excel_file = pd.ExcelFile(file)
             df1 = pd.read_excel(file, sheet_name=hoja1)
             df1 = df1.iloc[df1[df1.iloc[:, 0].eq('Id_Cliente')].index[0]:, :].reset_index(drop=True)
             df1.columns = df1.iloc[0]
+       
+            
+            df1.replace("", nan_value, inplace=True) 
+            df1.dropna(how='all', axis=0, inplace=True)
+            
             df1['ifc_mes']=mes
             df1['ifc_año']=anno
             df1 = df1.drop(0).reset_index(drop=True)
+        
             df1_total = df1_total.append(df1)
             df2 = excel_file.parse(sheet_name = hoja2, header=0)
-            df2.rename({'N°Cliente': 'Id_Cliente'}, axis=1,inplace=True)
+            df2.rename({'N°Cliente': 'Id_Cliente'}, axis=0,inplace=True)
+            
+            df2.replace("", nan_value, inplace=True) 
+            df2.dropna(how='all', axis=0, inplace=True)
+            
             df2['ifc_mes']=mes
             df2['ifc_año']=anno
             df2_total = df2_total.append(df2)
-            df3 = excel_file.parse(sheet_name = hoja3, header=0) 
+            df3 = excel_file.parse(sheet_name = hoja3, header=0)
+            
+            df3_total.replace("", nan_value, inplace=True) 
+            df3_total.dropna(how='all', axis=1, inplace=True)
+             
             df3['ifc_mes']=mes
             df3['ifc_año']=anno
             df3_total = df3_total.append(df3)
+            print('-------6-------')
+    print('-------7-------')
+    
+  
+    
     df1_total['RUT Cliente']=df1_total['RUT Cliente'].apply(limpieza_RUT)
     df1_total['RUT Suministrador']=df1_total['RUT Suministrador'].apply(limpieza_RUT)
     df1_total['RUT Receptor']=df1_total['RUT Receptor'].apply(limpieza_RUT)
     df1_total['RUT Distribuidor']=df1_total['RUT Distribuidor'].apply(limpieza_RUT)
     df2_total['RUT DISTRIBUIDORA']=df2_total['RUT DISTRIBUIDORA'].apply(limpieza_RUT)
     df3_total['RUT Distribuidor']=df3_total['RUT Distribuidor'].apply(limpieza_RUT)
-    df3_total['RUT Cliente']=df3_total['RUT Cliente'].apply(limpieza_RUT)    
+    df3_total['RUT Cliente']=df3_total['RUT Cliente'].apply(limpieza_RUT) 
+    
+    # se aplica diccionario
+    #df1_total=df1_total.apply(lambda a: a.strip())
+    #df2_total=df2_total.apply(lambda a: a.strip())
+    #df3_total=df3_total.apply(lambda a: a.strip())
+    
+    for section in diccionario.sections():
+        if section !='Tipo Proceso':
+            for elemento in diccionario[section]:
+                print(elemento)
+            #    print(section)
+                print(diccionario[section][elemento])
+                df1_total[section]=df1_total[section].replace(elemento, diccionario[section][elemento])
+                if section =='Distribuidor':
+                    df3_total[section]=df3_total[section].replace(elemento, diccionario[section][elemento])
+    # fin de diccionario 
+      
+    print('-------8-------') 
     archivo_errores = open(RUTA+"\errores_pjdx_"+mes+"_"+anno+".txt", "w")
     for section in configuracion.sections():
         tablas_maestras=[]
         errores=[]
         errores3=[]
         for elemento in configuracion[section]:
-                tablas_maestras.append(elemento)
+            tablas_maestras.append(elemento)
         archivo_errores.write("["+section+"]" + "\n")
         if section != 'Tipo Regimen':
             errores=set(list(df1_total[section]))-set(tablas_maestras)
@@ -166,11 +220,15 @@ def pjdx_mensual(mes,ruta,anno):
         for item in errores:
             archivo_errores.write("%s\n" % item)
         archivo_errores.write("\n\n")
+        #print(set(list(df1_total[section])))
+        #print(set(tablas_maestras))
     archivo_errores.close() 
     with ExcelWriter(RUTA+"\pjdx_"+mes+"_"+anno+".xlsx") as writer:
         df1_total.to_excel(writer, hoja1, index=False)
         df2_total.to_excel(writer, hoja2, index=False)
         df3_total.to_excel(writer, hoja3, index=False)
+    
+    
     print("Fin programa") 
     
 
